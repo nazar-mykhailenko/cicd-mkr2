f66544287b29e3dbc0dea417330225d540c21ba4
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-05-31 12:42:30 +0300

    Add views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.db.models import Count
+from .models import Author, Book
 
 # Create your views here.
+def main(request):
+    books = Book.objects.filter()[:5]
+    return render(request, 'book_list.html', {
+        'books': books,
+    })
+
+def book_list(request):
+    authors = Author.objects.annotate(book_count=Count('book'))
+    return render(request, 'author_list.html', {
+        'authors': authors
+    })
